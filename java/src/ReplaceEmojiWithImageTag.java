eee7ac06f884ebf79807197892a01b9ee9d5748e
VictorManduca <victor.manduca@hotmail.com>
2023-05-29 19:34:55 -0300

    refactor: improve code quality

--- java/src/ReplaceEmojiWithImageTag.java
+++ java/src/ReplaceEmojiWithImageTag.java
@@ -2,47 +2,36 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ReplaceEmojiWithImageTag {
-  private static final Map<String, String> emoji = new HashMap<>();
+	private static final Map<String, String> emoji = new HashMap<>();
 
-  static {
-    emoji.put(":smile:", "smile");
-    emoji.put(":heart:", "heart");
-  }
+	static {
+		emoji.put(":smile:", "smile");
+		emoji.put(":heart:", "heart");
+		emoji.put(":thumbs_up:", "thumbs_up");
+	}
 
-  private static String replaceEmojiWithImageTags(String input) {
-    StringBuilder result = new StringBuilder();
-    int startIndex = 0;
-    int endIndex;
+	private static String replaceEmojiWithImageTags(String input) {
+		int startIndex = 0;
 
-    while (startIndex < input.length()) {
-      int emojiStartIndex = input.indexOf(':', startIndex);
-      if (emojiStartIndex == -1) {
-        result.append(input.substring(startIndex));
-        break;
-      }
+		for (int i = 0; i < input.length(); i++) {
+			char currentChar = input.charAt(i);
 
-      int emojiEndIndex = input.indexOf(':', emojiStartIndex + 1);
-      if (emojiEndIndex == -1) {
-        result.append(input.substring(startIndex));
-        break;
-      }
+			if (currentChar == ':') {
+				if (startIndex == 0) {
+					startIndex = i;
+				} else {
+					String emojiName = input.substring(startIndex, i + 1);
+					String emojiHtmlElement = "<img src='" + emoji.getOrDefault(emojiName, "") + ".png'/>";
+					input = input.replace(emojiName, emojiHtmlElement);
+					startIndex = 0;
+				}
+			}
+		}
 
-      endIndex = emojiEndIndex + 1;
+		return input;
+	}
 
-      String emojiName = input.substring(emojiStartIndex, endIndex);
-      String imageUrl = emoji.getOrDefault(emojiName, "") + ".png";
-      String imgTag = "<img src='" + imageUrl + "'>";
-
-      result.append(input.substring(startIndex, emojiStartIndex));
-      result.append(imgTag);
-
-      startIndex = endIndex;
-    }
-
-    return result.toString();
-  }
-
-  public static void main(String[] args) {
-    System.out.println(replaceEmojiWithImageTags("I'm feeling :smile: today! :heart:"));
-  }
+	public static void main(String[] args) {
+		System.out.println(replaceEmojiWithImageTags("I'm feeling :smile: today! :heart:"));
+	}
 }
